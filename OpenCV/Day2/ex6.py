05ee875396e381e90f14c8acd839bce17317cc3c
Gongsoochan <140775642+Gongsoochan@users.noreply.github.com>
2023-09-14 11:26:03 +0900

    Delete OpenCV directory

--- OpenCV/Day2/ex6.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img=cv2.imread('cube.jpg')
-cv2.imshow("img",img)
-cv2.waitKey(0)
-t,tr_img=cv2.threshold(img[:,:,2],0,255,cv2.THRESH_BINARY)
-print(cv2.THRESH_BINARY)
-print(cv2.THRESH_OTSU)
-cv2.imshow("img",tr_img)
-cv2.waitKey(0)
--- OpenCV/Day2/ex9.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot
-img=cv2.imread('d3.jpg')
-cv2.imshow('img',img)
